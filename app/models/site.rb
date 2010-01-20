6c258a8ae34688f8db19f27bb4f6defb66a7a738
Benigno Uria <benigno.uria@gmail.com>
2010-01-20 23:53:32 +0000

    Set pagination to 5 sites per page. Destroy bookmarks associated when deleting a site.

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,10 @@
 class Site < ActiveRecord::Base
-  has_many :bookmarks
+  has_many :bookmarks, :dependent => :destroy
 
   validates_length_of :domain, :minimum => 1
+
+  def self.per_page
+    5
+  end
+
 end
